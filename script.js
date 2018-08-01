b05e6f1ae00a6253e4a6af246b5b0e83e4d3d6f9
dimati9 <dimati9@mail.ru>
2018-08-02 00:21:10 +0500

    project

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function show(id) {
+
+    let object = document.getElementById(id);
+    object.style.display == 'none' ? object.style.display = 'block' : object.style.display = 'none';
+
+}
+
+function hide(id) {
+    let object = document.getElementById(id);
+    object.style.display = 'none';
+
+}
+
+
+
+
+
+
+
+
